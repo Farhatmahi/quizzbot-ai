cd8a56b8cc31eaa9819ca489d61bafafda641616
Farhat Mahi <farhatmahi007@gmail.com>
2023-06-27 00:35:51 +0600

    [FIXED] Divider.jsx

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -5,7 +5,7 @@ import SocialMediaLogin from "@/components/SocialMediaLogin";
 export default function Home() {
   return (
     <main className="flex min-h-screen">
-      <div className="flex flex-col justify-center items-center p-8 bg-white w-1/2 lg:px-40">
+      <div className="flex flex-col justify-center items-center p-8 bg-white w-full lg:w-1/2 lg:px-24">
         <div className="flex justify-start w-full mb-10">
           <img src="./assets/logo.png" alt="" className="w-48" />
         </div>
