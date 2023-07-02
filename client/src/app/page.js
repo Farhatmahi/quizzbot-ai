52f6a0a1e3e8489cce9aa427176cfabbdf954b70
Ashraful Islam <19103189@iubat.edu>
2023-07-02 23:32:32 +0600

    home Ui more options added

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -1,11 +1,26 @@
 "use client";
 import HomeHeader from "@/components/HomePgae/HomeHeader";
 import AuthProvider from "@/context/AuthProvider";
+import Image from "next/image";
+import Banner from "../../public/assets/140shots_so.png";
+import HomeMiddle from "@/components/HomePgae/HomeMiddle";
 
 const page = () => {
   return (
     <>
       <HomeHeader />
+      {/* Hero Image */}
+      <Image
+        src={Banner}
+        width={200}
+        height={160}
+        sizes="(max-width: 768px) 100vw,
+              (max-width: 1200px) 50vw,
+              33vw"
+        style={{ height: "100%", width: "100%" }}
+      />
+
+      <HomeMiddle />
     </>
   );
 };
---
+++ client/src/components/HomePgae/HomeMiddle.jsx
@@ -0,0 +1,42 @@
+"use client";
+import Lottie from "lottie-react";
+import quiz from "../../../public/assets/quiz.json";
+import PrimaryButton from "../PrimaryButton";
+import Link from "next/link";
+
+const HomeMiddle = () => {
+  return (
+    <div className="lg:flex  lg:mx-20   lg:ml-0 ml-6">
+      <div className="text-[#0e0d0d] mt-6 lg:ml-32">
+        <p className="text-[16px] font-semibold uppercase">
+          AI Question Generator
+        </p>
+        <h1 className="text-[36px] font-bold mt-3">
+          Make life easier with Ai-QuizzBot
+        </h1>
+        <p className="text-[18px] mt-3">
+          AiQuizbot is a powerful AI question generator designed to
+          revolutionize the way you create questions and exams by allowing you
+          to create multiple versions of the test at different levels.
+        </p>
+        <Link href="/dashboard">
+          <button
+            class="inline-flex items-center 
+                justify-center ml-3 mb-12  mt-6 h-[46px] w-[220px] px-6 font-medium tracking-wide
+                 text-white transition rounded-2xl
+       duration-500 bg-gradient-to-r from-[#FC495F] from-62% via-[#FFc371] to-[#FF0000] to-38% 
+       bg-size-200 bg-pos-0 hover:bg-pos-100"
+            aria-label="Sign up"
+          >
+            Try Question for Free
+          </button>
+        </Link>
+      </div>
+      <div className="flex items-center  justify-center h-30 w-full lg:ml-56">
+        <Lottie animationData={quiz} loop={true}></Lottie>
+      </div>
+    </div>
+  );
+};
+
+export default HomeMiddle;
