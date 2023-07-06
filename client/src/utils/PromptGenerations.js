2e09dee5cf53083b24eb0b86ae92a15e4fb62d19
Istiakahmed180 <istiakahmed180@gmail.com>
2023-07-06 22:53:27 +0600

    lesson planer create prompt complete

--- client/src/utils/PromptGenerations.js
+++ client/src/utils/PromptGenerations.js
@@ -13,6 +13,8 @@ export const generatePrompt = ({
         """${content}"""
         `;
 
+  let lessonPlaner = `${content}`;
+
   switch (format) {
     case "true-false":
       prompt += `The response will be like this ${format} and answer
@@ -27,8 +29,10 @@ export const generatePrompt = ({
       ... So on 
 
       Answer responses in this format at the last not after the question
-      1. Answer: a) Give me the correct ${format} answer will be here - Give me the correct answer will be here.
-      2. Answer: b) Give me the correct ${format} answer will be here - Give me the correct answer will be here.
+
+      Answer:
+      1. a) Give me the correct True or False answer will be here - Give me the correct answer will be here.
+      2. b) Give me the correct True or False answer will be here - Give me the correct answer will be here.
       ... So on
         `;
       break;
@@ -51,8 +55,10 @@ export const generatePrompt = ({
       ... So on
     
       Answer responses in this format at the last not after the question
-      1. Answer: a) Give me the correct option answer will be here - Give me the correct answer will be here.
-      2. Answer: b) Give me the correct option answer will be here - Give me the correct answer will be here.
+
+      Answers:
+      1. a) Give me the correct option answer will be here - Give me the correct answer will be here.
+      2. b) Give me the correct option answer will be here - Give me the correct answer will be here.
       ... So on
         `;
       break;
@@ -66,9 +72,11 @@ export const generatePrompt = ({
       ... So on
       
       Answer And Explanation responses in this format at the last not after the question
-      1. Answer: {Question Number 1 Correct Answer will be here} - {Give me the explaination answer wiil be here},
-      2. Answer: {Question Number 2 Correct Answer will be here} - {Give me the explaination answer wiil be here},
-      3. Answer: {Question Number 3 Correct Answer will be here} - {Give me the explaination answer wiil be here},
+
+      Answer:
+      1. {Question Number 1 Correct Answer will be here} - {Give me the explaination answer wiil be here},
+      2. {Question Number 2 Correct Answer will be here} - {Give me the explaination answer wiil be here},
+      3. {Question Number 3 Correct Answer will be here} - {Give me the explaination answer wiil be here},
       ... So on
       `;
       break;
@@ -82,31 +90,25 @@ export const generatePrompt = ({
       break;
 
     case "matching":
-      prompt += `Handle matching questions format here`;
       break;
 
     case "calculations":
-      prompt += `Handle calculations format here`;
       break;
 
     case "lesson-planer":
-      prompt += `Handle ${format} format here`;
+      lessonPlaner += `In this text create lesson plan`;
       break;
 
     case "project-ideas":
-      prompt += `Handle ${format} format here`;
       break;
 
     case "study-points":
-      prompt += `Handle ${format} format here`;
       break;
 
     case "group-creator":
-      prompt += `Handle ${format} format here`;
       break;
 
     case "seating-chart":
-      prompt += `Handle ${format} format here`;
       break;
 
     default:
@@ -114,5 +116,5 @@ export const generatePrompt = ({
       break;
   }
 
-  return prompt;
+  return lessonPlaner;
 };
