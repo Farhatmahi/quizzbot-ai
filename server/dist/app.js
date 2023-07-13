6fea40f9791f8b97f2817ac5603e2207067b1d28
Farhat Mahi <farhatmahi007@gmail.com>
2023-07-13 18:02:39 +0600

    Build

--- server/dist/app.js
+++ server/dist/app.js
@@ -28,6 +28,14 @@ const corsOptions = {
 app.use((0, cors_1.default)(corsOptions));
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: true }));
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    res.setHeader('Access-Control-Max-Age', '1800');
+    res.setHeader('Access-Control-Allow-Headers', 'content-type');
+    res.setHeader('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, PATCH, OPTIONS');
+    next();
+});
 // Testing route
 app.get('/', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     const responseData = {
--- server/src/app.ts
+++ server/src/app.ts
@@ -18,6 +18,18 @@ app.use(cors(corsOptions))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Credentials', 'true')
+  res.setHeader('Access-Control-Max-Age', '1800')
+  res.setHeader('Access-Control-Allow-Headers', 'content-type')
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'PUT, POST, GET, DELETE, PATCH, OPTIONS'
+  )
+  next()
+})
+
 // Testing route
 app.get('/', async (req, res) => {
   const responseData = {
